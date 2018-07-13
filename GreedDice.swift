7f1a4f7161b186dfbe60b192c5f8de7037c169e7
Abraham Isaac Durán <aduran@oriontek.do>
2018-07-13 09:55:59 -0400

    Add GreedDiceTests class

--- GreedDice.swift
+++ GreedDice.swift
@@ -1,8 +1,18 @@
 //: Playground - noun: a place where people can play
 
 import Foundation
+import XCTest
 
 final class Greed {
 
 }
 
+final class GreedTests: XCTestCase {
+    private var _greed: Greed!
+    
+    override func setUp() {
+        super.setUp()
+        _greed = Greed()
+    }
+
+}
